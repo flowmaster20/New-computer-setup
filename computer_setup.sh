718693be23795293a77d4a9daa6d38b955b0e641
flowmaster20 <mkojro20@gmail.com>
2019-03-25 23:50:47 +0100

    czyszczenie po poleceniach - ladniej wyglada

--- computer_setup.sh
+++ computer_setup.sh
@@ -7,19 +7,33 @@ echo "#  |____/ |_/_/   \_\_| \_\|_|    #";
 echo "#                                 #";
 echo "###################################";
 sudo apt-get install snapd -y;
+cls
 sudo apt-get install git -y;
+cls
 sudo apt-get update -y;
+cls
 sudo apt-get upgrade -y;
+cls
 sudo apt-get install terminator -y;
+cls
 sudo apt-get install htop -y;
+cls
 sudo apt-get install iotop -y;
+cls
 sudo apt-get install tree -y;
+cls
 sudo snap install sublime-text;
+cls
 sudo snap install gitkraken;
+cls
 sudo apt-get install codeblocks -y;
+cls
 sudo apt-get install speedtest-cli -y;
+cls
 sudo snap install spotify;
+cls
 sudo apt-get install arduino -y;
+cls
 sudo apt-get install atom -y;
 
 ping google.com -c 5;
